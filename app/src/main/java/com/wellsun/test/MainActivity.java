cef700bb62ab9388d8fd9c9480c161c15b0128dc
zhaozheng <1114258696@qq.com>
2022-12-09 14:02:23 +0800

    语音合成

--- app/src/main/java/com/wellsun/test/MainActivity.java
+++ app/src/main/java/com/wellsun/test/MainActivity.java
@@ -4,10 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-
-import com.cczhr.TTS;
-import com.cczhr.TTSConstants;
 
 public class MainActivity extends AppCompatActivity {
 
