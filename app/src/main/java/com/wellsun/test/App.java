b90f9462c32832b002e5f333dd8a1d15cb743eed
zhaozheng <1114258696@qq.com>
2022-12-09 13:39:02 +0800

    语音合成

---
+++ app/src/main/java/com/wellsun/test/App.java
@@ -0,0 +1,16 @@
+package com.wellsun.test;
+
+import android.app.Application;
+
+/**
+ * date     : 2022-12-09
+ * author   : ZhaoZheng
+ * describe :
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
--- app/src/main/java/com/wellsun/test/MainActivity.java
+++ app/src/main/java/com/wellsun/test/MainActivity.java
@@ -3,6 +3,11 @@ package com.wellsun.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.cczhr.TTS;
+import com.cczhr.TTSConstants;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
